d2be97a0ea48acacf133b919a0b0d07702e27ee5
Munhoz <munhozfontana@gmail.com>
2021-03-22 11:21:23 -0300

    add style map

--- lib/features/presentation/find_bus_controller.dart
+++ lib/features/presentation/find_bus_controller.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:run_bus/core/params/params.dart';
+import 'package:run_bus/features/domain/entites/user_location.dart';
 import 'package:run_bus/features/domain/usecase/current_location_user_use_case.dart';
 
 class FindBusController extends ChangeNotifier {
   final CurrentLocationUserUseCase currentLocationUserUseCase;
+  UserLocation userLocation;
+  GoogleMapController googleMapController;
 
   FindBusController({@required this.currentLocationUserUseCase});
 
-  run() async {
+  onMapCreate(GoogleMapController controller) async {
+    googleMapController = controller;
+    googleMapController
+        .setMapStyle(await rootBundle.loadString('assets/map_style.txt'));
+    notifyListeners();
+  }
+
+  changeMap() async {
     var res = await currentLocationUserUseCase(Params());
-    res.fold((l) => null, (r) => print(r));
+
+    googleMapController.moveCamera(CameraUpdate.newLatLng(LatLng(
+      res.getOrElse(() => null).location.latitude,
+      res.getOrElse(() => null).location.longitude,
+    )));
+    notifyListeners();
+  }
+
+  void run() async {
+    var res = await currentLocationUserUseCase(Params());
+    print(res.getOrElse(() => null));
+    res.fold((l) => null, (r) => userLocation = r);
+    notifyListeners();
   }
 }
--- lib/features/presentation/find_bus_page.dart
+++ lib/features/presentation/find_bus_page.dart
@@ -3,23 +3,40 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:run_bus/features/presentation/find_bus_controller.dart';
 
-class FindBusPage extends StatelessWidget {
+class FindBusPage extends StatefulWidget {
   const FindBusPage({Key key}) : super(key: key);
+
+  @override
+  _FindBusPageState createState() => _FindBusPageState();
+}
+
+class _FindBusPageState extends State<FindBusPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.42796133580664, -122.085749655962),
-          zoom: 14.4746,
-        ),
+      body: Consumer<FindBusController>(
+        builder: (context, value, child) {
+          return GoogleMap(
+            onMapCreated: value.onMapCreate,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(
+                37.42796133580664,
+                -122.085749655962,
+              ),
+              zoom: 16,
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Provider.of<FindBusController>(context, listen: false)..run();
+          Provider.of<FindBusController>(context, listen: false)..changeMap();
         },
         child: Icon(Icons.add),
       ),
